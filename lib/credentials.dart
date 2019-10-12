2bf9107eddb762c24741a0819637c15c2f5811b7
Alba Lamas <alba.v.lamas@gmail.com>
2019-10-12 03:03:57 +0200

    trying to configure spotify api

---
+++ lib/credentials.dart
@@ -0,0 +1,4 @@
+class Credentials {
+  static String clientId = "9fc87d84db2a4e0ebf1351a2c88dcac2";
+  static String redirectUrl = "spotify-flutter://callback";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,318 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+import 'package:spotify_playback/spotify_playback.dart';
+import 'credentials.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _MyAppState extends State<MyApp> {
+  bool _connectedToSpotify = false;
+  Uint8List image = null;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  void initState() {
+    super.initState();
+    initConnector();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  /// Initialize the spotify playback sdk, by calling spotifyConnect
+  Future<void> initConnector() async {
+    try {
+      await SpotifyPlayback.spotifyConnect(
+              clientId: Credentials.clientId,
+              redirectUrl: Credentials.redirectUrl)
+          .then((connected) {
+        if (!mounted) return;
+        // If the method call is successful, update the state to reflect this change
+        setState(() {
+          _connectedToSpotify = connected;
+        });
+      }, onError: (error) {
+        // If the method call trows an error, print the error to see what went wrong
+        print(error);
+      });
+    } on PlatformException {
+      print('Failed to connect.');
+    }
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  /// Play an song by spotify track/album/playlist id
+  Future<void> play(String id) async {
+    try {
+      await SpotifyPlayback.play(id).then((success) {
+        print(success);
+      }, onError: (error) {
+        print(error);
+      });
+    } on PlatformException {
+      print('Failed to play.');
+    }
+  }
+
+  /// Pause the currently playing track
+  Future<void> pause() async {
+    try {
+      await SpotifyPlayback.pause().then((success) {
+        print(success);
+      }, onError: (error) {
+        print(error);
+      });
+    } on PlatformException {
+      print('Failed to pause.');
+    }
+  }
+
+  /// Resume the currently paused track
+  Future<void> resume() async {
+    try {
+      await SpotifyPlayback.resume().then((success) {
+        print(success);
+      }, onError: (error) {
+        print(error);
+      });
+    } on PlatformException {
+      print('Failed to resume.');
+    }
+  }
+
+  /// Add an song / playlist / album to the playback queue
+  Future<void> queue(String id) async {
+    try {
+      await SpotifyPlayback.queue(id).then((success) {
+        print(success);
+      }, onError: (error) {
+        print(error);
+      });
+    } on PlatformException {
+      print('Failed to queue.');
+    }
+  }
+
+  /// Seek to a defined time in a song
+  Future<void> seekTo() async {
+    try {
+      await SpotifyPlayback.seekTo(20000).then((success) {
+        print(success);
+      }, onError: (error) {
+        print(error);
+      });
+    } on PlatformException {
+      print('Failed to play.');
+    }
+  }
+
+  /// Seek to a a defined time relative to the current time
+  Future<void> seekToRelativePosition() async {
+    try {
+      await SpotifyPlayback.seekToRelativePosition(5000).then((success) {
+        print(success);
+      }, onError: (error) {
+        print(error);
+      });
+    } on PlatformException {
+      print('Failed to play.');
+    }
+  }
+
+  /// Gets the currently playing track's playback position
+  Future<void> getPlaybackPosition() async {
+    try {
+      await SpotifyPlayback.getPlaybackPosition().then((position) {
+        print(
+            "playback position -->\nmin:${DateTime.fromMillisecondsSinceEpoch(position).minute}\nseconds:${DateTime.fromMillisecondsSinceEpoch(position).second}");
+        return position;
+      }, onError: (error) {
+        print(error);
+      });
+    } on PlatformException {
+      print('Failed to resume.');
+    }
+  }
+
+  /// Gets the currently playing track information
+  Future<void> getCurrentlyPlayingTrack() async {
+    try {
+      SpotifyPlayback.getCurrentlyPlayingTrack().then((track) {
+        print("track:\n$track");
+      });
+    } on PlatformException {
+      print('Failed to resume.');
+    }
+  }
+
+  /// Sets an listener on the current track's playback position
+  /// Needs changes and is currently not in use
+  @deprecated
+  Future<void> monitorPlaybackPosition() async {
+    try {
+      print("Enable timer");
+      SpotifyPlayback.enableTimer().listen((val) {
+        print("track:\n$val");
+      }, onError: (error) {
+        print(error);
+      });
+    } on PlatformException {
+      print('Failed to resume.');
+    }
+  }
+
+  /// Play the previous song
+  Future<void> skipPrevious() async {
+    try {
+      await SpotifyPlayback.skipPrevious().then((success) {
+        print(success);
+      }, onError: (error) {
+        print(error);
+      });
+    } on PlatformException {
+      print('Failed to play.');
+    }
+  }
+
+  ///Play the next song
+  Future<void> skipNext() async {
+    try {
+      await SpotifyPlayback.skipNext().then((success) {
+        print(success);
+      }, onError: (error) {
+        print(error);
+      });
+    } on PlatformException {
+      print('Failed to play next song.');
+    }
+  }
+
+  /// Toggle shuffle
+  Future<void> toggleShuffle() async {
+    try {
+      await SpotifyPlayback.toggleShuffle().then((success) {
+        print(success);
+      }, onError: (error) {
+        print(error);
+      });
+    } on PlatformException {
+      print('Failed to toggle shuffle.');
+    }
+  }
+
+  /// Toggle repeat
+  Future<void> toggleRepeat() async {
+    try {
+      await SpotifyPlayback.toggleRepeat().then((success) {
+        print(success);
+      }, onError: (error) {
+        print(error);
+      });
+    } on PlatformException {
+      print('Failed to toggle repeat.');
+    }
+  }
+
+  /// Get the image by provided uri
+  Future<void> getImage(String uri) async {
+    try {
+      await SpotifyPlayback.getImage(uri: uri, quality: 100, size: 100).then(
+          (imageReceived) {
+        setState(() {
+          image = imageReceived;
+        });
+      }, onError: (error) {
+        print(error);
+      });
+    } on PlatformException {
+      print('Failed to play.');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        return MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Spotify plugin example app'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+            body: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                    child: Text('Connected to spotify: $this._connectedToSpotify\n'),
+              ),
+              Wrap(
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () =>
+                        play("spotify:track:3HVWdVOQ0ZA45FuZGSfvns"),
+                    child: Text("Play"),
+                  ),
+                  RaisedButton(
+                    onPressed: () => resume(),
+                    child: Text("Resume"),
+                  ),
+                  RaisedButton(
+                    onPressed: () => pause(),
+                    child: Text("Pause"),
+                  ),
+                  RaisedButton(
+                    onPressed: () =>
+                        queue("spotify:track:6b6liLOZhXdOfFBFVN5KNK"),
+                    child: Text("Queue"),
+                  ),
+                  RaisedButton(
+                    onPressed: () => skipPrevious(),
+                    child: Text("Prev"),
+                  ),
+                  RaisedButton(
+                    onPressed: () => skipNext(),
+                    child: Text("Next"),
+                  ),
+                  RaisedButton(
+                    onPressed: () => toggleShuffle(),
+                    child: Text("Shuffle"),
+                  ),
+                  RaisedButton(
+                    onPressed: () => toggleRepeat(),
+                    child: Text("Repeat"),
+                  ),
+                  RaisedButton(
+                    onPressed: () => getPlaybackPosition(),
+                    child: Text("Position"),
+                  ),
+                  RaisedButton(
+                    onPressed: () => monitorPlaybackPosition(),
+                    child: Text("MonitorPlaybackPosition"),
+                  ),
+                  RaisedButton(
+                    onPressed: () => seekTo(),
+                    child: Text("Seek"),
+                  ),
+                  RaisedButton(
+                    onPressed: () => seekToRelativePosition(),
+                    child: Text("Seek Relative(+5s)"),
+                  ),
+                  RaisedButton(
+                      onPressed: () => getImage(
+                            SpotifyPlayback.imageLinkToURi(
+                                "https://i.scdn.co/image/3269971d34d3f17f16efc2dfa95e302cc961a36c"),
+                          ),
+                      child: Text("Get image")),
+
+                  (image != null)
+                      ? Image.memory(
+                          image,
+                          height: 100,
+                        )
+                      : Text("Image Placeholder")
+                ],
+              ),
+            ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
